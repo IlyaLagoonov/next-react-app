b72bf6cbaa113b269b91eef09594ae8dc661245e
IlyaLagoonov <iliay338@gmail.com>
2022-10-23 16:43:30 +0300

    add button and styles

--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -1,10 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import {Htag} from "../components";
+import {Button, Htag} from "../components";
 
 
 export default function Home():JSX.Element {
   return (
-        <Htag tag='h1'>Главный тут тег</Htag>
+      <>
+          <Htag tag='h1'>Главный тут тег</Htag>
+          <Button appearance={'primary'}>
+              Кнопуска
+        </Button>
+
+          <Button appearance={'ghost'} >
+              Кнопуска вторая(проверочная)
+          </Button>
+      </>
+
+
   );
 }
