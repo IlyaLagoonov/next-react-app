373c4435969c0b67ad8c6c686a20708e6448cd0a
IlyaLagoonov <iliay338@gmail.com>
2022-10-31 18:43:11 +0300

    added layout HOC

---
+++ my-next-app/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import {FooterProps} from "./Footer.props";
+import styles from "./Footer.module.css";
+
+export const Footer = ({ ...props }: FooterProps):JSX.Element => {
+   return (
+       <footer>
+           <div>
+               футер
+           </div>
+       </footer>
+   )
+};
