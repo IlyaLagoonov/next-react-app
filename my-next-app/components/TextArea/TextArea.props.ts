64cce9696f8eb3bafcdc88c933634fbbc32f7aac
IlyaLagoonov <iliay338@gmail.com>
2022-12-26 20:21:39 +0300

    add new component product, search, adv. Added grid for product on page

---
+++ my-next-app/components/TextArea/TextArea.props.ts
@@ -0,0 +1,5 @@
+import {ReactNode,DetailedHTMLProps,TextareaHTMLAttributes} from "react";
+
+export interface TextAreaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>,HTMLTextAreaElement>{
+
+}
